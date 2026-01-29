4138a0943e662cafcdccdca54ce9b505570bbfba
Maksim Lin <maks@manichord.com>
2026-01-29 14:46:50 +1100

    make log output text selectable for copy/paste

--- data/core/logview.lua
+++ data/core/logview.lua
@@ -1,16 +1,92 @@
 local core = require "core"
 local style = require "core.style"
-local View = require "core.view"
+local Doc = require "core.doc"
+local DocView = require "core.docview"
 
 
-local LogView = View:extend()
+local function split_lines(text)
+  local res = {}
+  for line in (text .. "\n"):gmatch("(.-)\n") do
+    table.insert(res, line .. "\n")
+  end
+  if #res == 0 then
+    res[1] = "\n"
+  end
+  return res
+end
 
+
+local function append_lines(lines, prefix, text)
+  local first = true
+  local pad = string.rep(" ", #prefix)
+  for line in (text .. "\n"):gmatch("(.-)\n") do
+    if line ~= "" or first then
+      table.insert(lines, (first and prefix or pad) .. line)
+      first = false
+    end
+  end
+end
+
+
+local function build_log_text()
+  local lines = {}
+  for i = #core.log_items, 1, -1 do
+    local item = core.log_items[i]
+    local time = os.date(nil, item.time)
+    local prefix = time .. " "
+    append_lines(lines, prefix, item.text)
+    if item.info then
+      table.insert(lines, string.rep(" ", #prefix) .. "at " .. item.at)
+      append_lines(lines, string.rep(" ", #prefix), item.info)
+    end
+  end
+  return table.concat(lines, "\n")
+end
+
+
+local LogDoc = Doc:extend()
+
+function LogDoc:get_name()
+  return "Log"
+end
+
+function LogDoc:insert()
+end
+
+function LogDoc:remove()
+end
+
+function LogDoc:text_input()
+end
+
+function LogDoc:replace()
+end
+
+function LogDoc:delete_to()
+end
+
+function LogDoc:undo()
+end
+
+function LogDoc:redo()
+end
+
+function LogDoc:set_text(text)
+  local selection = { self:get_selection() }
+  self:reset()
+  self.lines = split_lines(text)
+  self.highlighter:invalidate(1)
+  self:clean()
+  self:set_selection(table.unpack(selection))
+end
+
+
+local LogView = DocView:extend()
 
 function LogView:new()
-  LogView.super.new(self)
-  self.last_item = core.log_items[#core.log_items]
-  self.scrollable = true
-  self.yoffset = 0
+  LogView.super.new(self, LogDoc())
+  self.font = "font"
+  self.last_item = nil
 end
 
 
@@ -19,55 +95,24 @@ function LogView:get_name()
 end
 
 
+function LogView:get_gutter_width()
+  return style.padding.x
+end
+
+
+function LogView:draw_line_gutter()
+end
+
+
 function LogView:update()
   local item = core.log_items[#core.log_items]
   if self.last_item ~= item then
     self.last_item = item
+    self.doc:set_text(build_log_text())
     self.scroll.to.y = 0
-    self.yoffset = -(style.font:get_height() + style.padding.y)
   end
-
-  self:move_towards("yoffset", 0)
 
   LogView.super.update(self)
-end
-
-
-local function draw_text_multiline(font, text, x, y, color)
-  local th = font:get_height()
-  local resx, resy = x, y
-  for line in text:gmatch("[^\n]+") do
-    resy = y
-    resx = renderer.draw_text(style.font, line, x, y, color)
-    y = y + th
-  end
-  return resx, resy
-end
-
-
-function LogView:draw()
-  self:draw_background(style.background)
-
-  local ox, oy = self:get_content_offset()
-  local th = style.font:get_height()
-  local y = oy + style.padding.y + self.yoffset
-
-  for i = #core.log_items, 1, -1 do
-    local x = ox + style.padding.x
-    local item = core.log_items[i]
-    local time = os.date(nil, item.time)
-    x = renderer.draw_text(style.font, time, x, y, style.dim)
-    x = x + style.padding.x
-    local subx = x
-    x, y = draw_text_multiline(style.font, item.text, x, y, style.text)
-    renderer.draw_text(style.font, " at " .. item.at, x, y, style.dim)
-    y = y + th
-    if item.info then
-      subx, y = draw_text_multiline(style.font, item.info, subx, y, style.dim)
-      y = y + th
-    end
-    y = y + style.padding.y
-  end
 end
 
 
